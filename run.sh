5c662af653f295b088ba1364a5b291aaf835a491
RumitAP <rumit97@gmail.com>
2022-11-10 01:20:38 -0500

    updated script

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 cd /home/cs6400-2022-03-Team044/
 npm install
 service postgresql start
-service postgresql initdb
-service postgresql start
 sudo -u postgres psql -c 'create database cs6400;'
 sudo -u postgres psql -d cs6400  -a -f Phase2/team044_p2_schema.sql
 
